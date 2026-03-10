ea2c5974db10e5500d53b08f5414c5462548458d
Abdul Aziz <aziz20031110@gmail.com>
2026-03-11 01:14:34 +0430

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen space-y-4 bg-blue-50 p-8">
+      <h1 className="text-4xl font-zentry">Zentry Font</h1>
+      <p className="text-xl font-general">General Font</p>
+      <p className="text-xl font-circular-web">Circular Web Font</p>
+      <p className="text-xl font-robert-medium">Robert Medium Font</p>
+      <p className="text-xl font-robert-regular">Robert Regular Font</p>
+    </div>
+  );
+};
+
+export default App;
