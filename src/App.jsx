52deff33d12fadb509095c1b3688646f6ea05287
Abdul Aziz <aziz20031110@gmail.com>
2026-03-11 18:42:59 +0430

    hero section added with its animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div className="min-h-screen space-y-4 bg-blue-50 p-8">
-      <h1 className="text-4xl font-zentry">Zentry Font</h1>
-      <p className="text-xl font-general">General Font</p>
-      <p className="text-xl font-circular-web">Circular Web Font</p>
-      <p className="text-xl font-robert-medium">Robert Medium Font</p>
-      <p className="text-xl font-robert-regular">Robert Regular Font</p>
-    </div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+
+      <section className="z-0 min-h-screen bg-blue-500" />
+    </main>
   );
 };
 
